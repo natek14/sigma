16c89784ae6c6098e66da358d4cc8332c2f9131d
natek14 <natanielmirota1405@gmail.com>
2026-03-23 12:29:25 +0100

    Create install script for Arch Linux setup
    This script sets up an Arch Linux environment by configuring NTP, setting up partitions, formatting, mounting, installing base packages, and configuring GRUB. It also creates a user and sets up a Git repository.

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+timedatectl set-ntp true
+reflector --country Poland --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
+echo -e "g\nn\n1\n\n+512M\nt\n1\n1\nn\n2\n\n\nw" | fdisk /dev/nvme0n1
+mkfs.fat -F32 /dev/nvme0n1p1
+mkfs.ext4 /dev/nvme0n1p2
+mount /dev/nvme0n1p2 /mnt
+mount --mkdir /dev/nvme0n1p1 /mnt/boot
+pacstrap -K /mnt base linux linux-firmware grub efibootmgr git sudo nano
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt bash <<EOF
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+useradd -m -G wheel natek
+passwd -d natek
+passwd -d root
+echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+cd /home/natek
+sudo -u natek git clone https://github.com
+cd Arch-Hyprland
+chmod +x install.sh
+sudo -u natek ./install.sh
+EOF
+echo "GOTOWE - WPISZ REBOOT"
